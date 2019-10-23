80c60369edadbd43d07509268e777b718899cc72
lijin liu <llj098@gmail>
2019-10-23 08:34:51 +0800

    add binance

---
+++ src/brokers/binance.py
@@ -0,0 +1,216 @@
+# coding: utf8
+
+"""Implement GDAX broker."""
+
+import os
+import pandas as pd
+import requests
+import sys
+import tailer
+import time
+
+
+class Binance(object):
+    """Represents GDAX broker."""
+
+    # URL to retrieve trades
+    BASE_URL = 'https://api.binance.com'
+    ALLOWED_PAIRS = ['BTCUSDT']
+
+    # Max trades retrievable by Binance on one request
+    LIMIT = 1000
+
+    @classmethod
+    def get_trades(cls, base_trade_number, pair):
+        """Return all trades between base_trade_number and
+           base_trade_number + 99.
+
+        Positional arguments:
+        base_trade_number -- The base trade number
+        pair              -- The pair to trade
+
+        Returns a tuple with the following shape
+        (list of dictionnaries, boolean)
+
+        The list of dictionnaries contains dictionnaries with the following
+        shape:
+        {
+          'time': '015-04-29T04:55:54.675974Z',
+          'trade_id': 42,
+          'price': 206.13,
+          'size': 0.1037,
+          'side': 'sell',
+          'last': False
+        }
+
+        The boolean is True is this list contain the last known trade, else is
+        False.
+
+        Raise a Runtime Error if problem during the request.
+        """
+        after = base_trade_number + cls.LIMIT
+
+        url = '{}{}?symbol={}&fromId={}&limit={}'.format(cls.BASE_URL, '/api/v1/aggTrades', pair, base_trade_number, cls.LIMIT)
+        print(url)
+        response = requests.get(url)
+
+        # Raise if error
+        status_code = response.status_code
+        if response.status_code != 200:
+            message = ("Error code " + str(status_code) +
+                       " for base trade number " + str(base_trade_number) +
+                       'body ' + response.content)
+            raise RuntimeError(message)
+
+        trades = response.json()
+        last = len(trades) == 0
+
+        # Test if these trades contain the most recent one
+        #not trades[0]['a'] == base_trade_number + cls.LIMIT - 1
+
+        # If it is the case, delete trades older than base_trade_number
+        if last:
+            filt_trades = [trade for trade in trades
+                           if float(trade['trade_id']) >= base_trade_number]
+        else:
+            filt_trades = trades
+
+        #NOTE: side is taker side
+        return ([dict(trade_id=trade['a'], size=float(trade['q']),
+                      side="sell" if trade['m'] else 'buy', price=float(trade['p']),
+                      time=trade['T']) for trade in trades], last)
+
+    @classmethod
+    def write_trades_from(cls, base_trade_id, file_path, pair):
+        """Write in the file 'file_path' all trades from base_trade_id.
+
+        Output file is a CSV file with the following columns:
+        trade_id, price, side, volume, date
+
+        Positional arguments:
+        base_trade_id -- The ID of the best trade
+        file_path     -- The file where trades should be written
+        pair          -- The pair to trade
+        """
+        is_last_trade = False
+        current_base_trade = base_trade_id + 1
+
+        write_header = not os.path.isfile(file_path)
+
+        while not is_last_trade:
+            current_last_trade = current_base_trade + cls.LIMIT - 1
+            msg = ('Get trades ' + str(current_base_trade) + ' to ' +
+                   str(current_last_trade) + "...")
+            sys.stdout.write(msg)
+            sys.stdout.flush()
+            try:
+                trades, is_last_trade = cls.get_trades(current_base_trade,
+                                                       pair)
+
+                # Return if no trade detected (could happen if this program is
+                # called when no new trade is availabled since the last one in
+                # the output file)
+                if not trades:
+                    return
+
+                current_base_trade += len(trades)
+                sys.stdout.write(" OK\n")
+                sys.stdout.flush()
+
+                df = pd.DataFrame.from_records(trades, index='trade_id')
+                df.sort_index(inplace=True)
+                df.to_csv(file_path, mode='a', header=write_header)
+                write_header = False
+            except RuntimeError as err:
+                sys.stdout.write(" KO\n")
+                print(err)
+                sys.stdout.flush()
+            except requests.exceptions.HTTPError as errh:
+                print ("Http Error:",errh)
+                time.sleep(3)
+                sys.stdout.flush()
+            except requests.exceptions.ConnectionError as errc:
+                print ("Error Connecting:",errc)
+                time.sleep(3)
+                sys.stdout.flush()
+            except requests.exceptions.Timeout as errt:
+                print ("Timeout Error:",errt)
+                time.sleep(3)
+                sys.stdout.flush()
+            except requests.exceptions.RequestException as err:
+                print ("OOps: Something Else",err)
+                time.sleep(3)
+                sys.stdout.flush()
+            except:
+                print ("OOps: Something Else", sys.exc_info()[0])
+                raise
+                time.sleep(3)
+                sys.stdout.flush()
+
+
+
+    @staticmethod
+    def get_last_trade_id_of_file(file_path):
+        """Return the last trade ID written in the file file_path.
+
+        Positional arguments:
+        file_path -- The CSV file to check
+        """
+        try:
+            last_line = tailer.tail(open(file_path, 'r'), 1)[0]
+            return int(last_line.split(',')[0])
+        except IOError:
+            return 0
+        except ValueError:
+            message = ('The file ' + file_path + ' seems corrupted. Please ' +
+                       'take a look.')
+            raise ValueError(message)
+
+    @staticmethod
+    def check_file_consistency(file_path):
+        """Return a list containing the trade ID where a issue is detected in
+        the file file_path.
+
+        Positional arguments:
+        file_path -- The file to check
+        """
+        tr_ids = pd.read_csv(file_path, usecols=['trade_id'])
+        return tr_ids[tr_ids.diff()['trade_id'] != 1]['trade_id'].tolist()[1:]
+
+    @classmethod
+    def print_check_file_consistency(cls, file_path):
+        """Check the consistency of the file file_path and print error message
+        on console if needed.
+
+        Positional arguments:
+        file_path -- The file to check
+        """
+        diffs = cls.check_file_consistency(file_path)
+
+        if diffs:
+            print('Errors detected in file "' + file_path + '" at trades ' +
+                  str(diffs)[1:-1])
+            print 'Please fix this file manually'
+            return True
+        else:
+            print 'No error detected in "' + file_path + '"'
+            return False
+
+    @classmethod
+    def download_missing_trades(cls, out_f, pair):
+        """Download the missing trades.
+
+        Positional arguments:
+        out_f -- The file where trades should be written
+        pair  -- The pair to trade
+        """
+
+        # Check output file consistency
+        if os.path.isfile(out_f) and cls.print_check_file_consistency(out_f):
+            sys.exit()
+
+        # Find the last retrieved trade if exists
+        last_trade = cls.get_last_trade_id_of_file(out_f)
+
+        # Download missing trades
+        cls.write_trades_from(last_trade, out_f, pair)
